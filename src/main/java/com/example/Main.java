4bd530a0d59da768fbe504b6454f4928a9569409
Azukirice <kzksx_180@yahoo.co.jp>
2023-12-08 23:49:19 +0900

    hello worldプログラムの作成

---
+++ src/main/java/com/example/Main.java
@@ -0,0 +1,14 @@
+package com.example;
+
+public class Main {
+    public static void main(String[] args) {
+        //i<11ならばhelloworld,i>10ならばhelloを表示する
+        for (int i = 0; i < 20; i++) {
+            if (i < 11) {
+                System.out.println("hello world");
+            } else {
+                System.out.println("hello");
+            }
+        }
+    }
+}
